7f91b568beb2399fbc35bf8e5c8205098ac0c4da
Yılmaz Yağız Dokumacı <dokumaciyagiz@gmail.com>
2018-08-20 00:46:24 +0300

    Çarpım tablosu v2

--- Program/src/program/Program.java
+++ Program/src/program/Program.java
@@ -7,12 +7,14 @@ public class Program {
         System.out.println("Java Ödev - Hoca : Mert Acel");
 
         System.out.println("Çarpım Tablosu");
+        
+
 
         for (int i = 1; i <= 10; i++) {
-
+ System.out.println("-------------------------------");
             for (int j = 1; j <= 10; j++) {
-
-                System.out.print(i + " x " + j + " = " + (i * j));
+       
+                System.out.println(i + " x " + j + " = " + (i * j));
 
             }
 
