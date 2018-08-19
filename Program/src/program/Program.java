1436a02db4f1dc7cdcac1199666c1ef4a65fff2f
Yılmaz Yağız Dokumacı <dokumaciyagiz@gmail.com>
2018-08-20 00:50:03 +0300

    Java Çarpım Tablosu

--- Program/src/program/Program.java
+++ Program/src/program/Program.java
@@ -11,7 +11,7 @@ public class Program {
 
 
         for (int i = 1; i <= 10; i++) {
- System.out.println("-------------------------------");
+ System.out.println("---------------------------------");
             for (int j = 1; j <= 10; j++) {
        
                 System.out.println(i + " x " + j + " = " + (i * j));
