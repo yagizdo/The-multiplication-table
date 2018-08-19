a4fd2d4cfbf93c167d8f750d9f2124f9744bc761
Yılmaz Yağız Dokumacı <dokumaciyagiz@gmail.com>
2018-08-20 00:42:47 +0300

    Java ile çarpım tablosu.

---
+++ Program/src/program/Program.java
@@ -0,0 +1,23 @@
+package program;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        System.out.println("Java Ödev - Hoca : Mert Acel");
+
+        System.out.println("Çarpım Tablosu");
+
+        for (int i = 1; i <= 10; i++) {
+
+            for (int j = 1; j <= 10; j++) {
+
+                System.out.print(i + " x " + j + " = " + (i * j));
+
+            }
+
+        }
+
+    }
+
+}
