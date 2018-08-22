1dedf55ca8cc609d45901656ae11567e4d114fa1
Yılmaz Yağız Dokumacı <dokumaciyagiz@gmail.com>
2018-08-22 17:47:33 +0300

    Java ile çarpım tablosu V2

--- Program/src/program/Program.java
+++ Program/src/program/Program.java
@@ -4,16 +4,14 @@ public class Program {
 
     public static void main(String[] args) {
 
-        System.out.println("Java Ödev - Hoca : Mert Acel");
+        System.out.println("Java Ödev \n Hoca : Mert Acel");
 
         System.out.println("Çarpım Tablosu");
-        
-
 
         for (int i = 1; i <= 10; i++) {
- System.out.println("---------------------------------");
+            System.out.println("---------------------------------\n---------------------------------");
             for (int j = 1; j <= 10; j++) {
-       
+
                 System.out.println(i + " x " + j + " = " + (i * j));
 
             }
